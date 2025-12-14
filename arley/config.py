d00ca40cea79bf4ddb46612882b69dbd01df6328
Henning Thieß <ht@xomox.cc>
2025-12-14 17:13:03 +0100

    Update logger configuration in `config.py` to improve formatting and add filtering options

--- arley/config.py
+++ arley/config.py
@@ -28,14 +28,14 @@ from pydantic_settings import (BaseSettings, DotEnvSettingsSource,
 # https://buildmedia.readthedocs.org/media/pdf/loguru/latest/loguru.pdf
 os.environ["LOGURU_LEVEL"] = os.getenv("LOGURU_LEVEL", "DEBUG")  # standard is DEBUG
 logger.remove()  # remove default-handler
-logger_fmt: str = (
-    "<g>{time:HH:mm:ssZZ}</> | <lvl>{level}</> | <c>{module}::{extra[classname]}:{function}:{line}</> - {message}"
-)
-#
-logger.add(
-    sys.stderr, level=os.getenv("LOGURU_LEVEL"), format=logger_fmt  # type: ignore
-)  # TRACE | DEBUG | INFO | WARN | ERROR |  FATAL
-logger.configure(extra={"classname": "None"})
+logger_fmt: str = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{module}</cyan>::<cyan>{extra[classname]}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+# logger_fmt: str = "<g>{time:HH:mm:ssZZ}</> | <lvl>{level}</> | <c>{module}::{extra[classname]}:{function}:{line}</> - {message}"
+
+def _loguru_skiplog_filter(record: dict) -> bool:
+    return not record.get("extra", {}).get("skiplog", False)
+
+logger.add(sys.stderr, level=os.getenv("LOGURU_LEVEL"), format=logger_fmt, filter=_loguru_skiplog_filter)  # type: ignore # TRACE | DEBUG | INFO | WARN | ERROR |  FATAL
+logger.configure(extra={"classname": "None", "skiplog": False})
 
 
 logger.info(f"EFFECTIVE CONFIGPATH: {_CONFIGPATH}")
